f298a6bcdb5ce329db185e332b3061c0160f0f13
root <root@master.vm>
2017-10-02 13:57:44 +0000

    removed softlink

--- examples/nginx/manifests/init.pp
+++ examples/nginx/manifests/init.pp
@@ -1,1 +1,25 @@
-init.pp.1
+Service {
+  provider => dummy
+}
+
+class { 'nginx': }
+
+nginx::resource::vhost { 'default':
+  www_root => '/var/www/html',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  source  => 'puppet:///modules/puppetconf/index.html',
+}
+
+file { '/var/www/html/puppet-docker.png':
+  ensure  => file,
+  source  => 'puppet:///modules/puppetconf/puppet-docker.png',
+}
+
+exec { 'Disable Nginx daemon mode':
+  path    => '/bin',
+  command => 'echo "daemon off;" >> /etc/nginx/nginx.conf',
+  unless  => 'grep "daemon off" /etc/nginx/nginx.conf',
+}
