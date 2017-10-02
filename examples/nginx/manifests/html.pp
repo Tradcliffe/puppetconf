861e2f2b6d37da20827b46992d55b268327773c0
root <root@master.vm>
2017-10-02 15:09:27 +0000

    html hacked

--- examples/nginx/manifests/html.pp
+++ examples/nginx/manifests/html.pp
@@ -10,7 +10,7 @@ nginx::resource::vhost { 'default':
 
 file { '/var/www/html/index.html':
   ensure  => present,
-  content => '<html><body><center><img src="http://photos3.meetupstatic.com/photos/event/4/6/7/7/600_458778039.jpeg" align=center></center></body></html>',
+  content => '<html><body><center><img src="https://puppet.com/sites/default/files/2016-10/Screen%20Shot%202016-10-15%20at%201.25.07%20pm_0.png" align=center></center></body></html>',
 }
 
 exec { 'Disable Nginx daemon mode':
